00c6c0b670ff89eac3da4ad7e9eb95ea283ec9c5
karevboris <karevboris333@gmail.com>
2019-03-17 19:24:30 +0300

    first commit

---
+++ PolynomialUnitTest.py
@@ -0,0 +1,98 @@
+import unittest
+from polynomial import Polynomial
+
+
+class PolynomialTestCase(unittest.TestCase):
+    def setUp(self):
+        self.p0 = Polynomial([0, ])
+        self.p1 = Polynomial([1, 2, 3])
+        self.p2 = Polynomial([1, -2, 3, 10])
+        self.p3 = Polynomial([-1, 0, 2, -5])
+
+    def test_init(self):
+        self.assertEqual(self.p1.coeffs, [1, 2, 3])
+
+    def test_init_error(self):
+        self.assertRaises(ValueError, Polynomial, [1, 'efw'])
+
+    def test_init_empty(self):
+        self.assertEqual(self.p0, Polynomial([]))
+
+    def test_repr(self):
+        self.assertEqual(self.p1.__repr__(), "Polynomial([1, 2, 3])")
+
+    def test_str(self):
+        self.assertEqual(self.p3.__str__(), "- x^3 + 2x - 5")
+
+    def test_sum_polynoms(self):
+        self.assertEqual(self.p1 + self.p2, Polynomial([1, -1, 5, 13]))
+
+    def test_sum_error(self):
+        self.assertRaises(ValueError, self.p1.__add__, "x")
+
+    def test_sum_number(self):
+        self.assertEqual(self.p1 + 100, Polynomial([1, 2, 103]))
+
+    def test_rsum_number(self):
+        self.assertEqual(100 + self.p1, Polynomial([1, 2, 103]))
+
+    def test_rsum_error(self):
+        self.assertRaises(ValueError, self.p1.__radd__, "x")
+
+    def test_sub_polynoms(self):
+        self.assertEqual(self.p1 - self.p2, Polynomial([-1, 3, -1, -7]))
+
+    def test_sub_number(self):
+        self.assertEqual(self.p1 - 100, Polynomial([1, 2, -97]))
+
+    def test_sub_error(self):
+        self.assertRaises(ValueError, self.p1.__sub__, "x")
+
+    def test_rsub_number(self):
+        self.assertEqual(100 - self.p1, Polynomial([-1, -2, 97]))
+
+    def test_rsub_error(self):
+        self.assertRaises(ValueError, self.p1.__rsub__, "x")
+
+    def test_mul_number(self):
+        self.assertEqual(self.p1 * 100, Polynomial([100, 200, 300]))
+
+    def test_mul_polynoms(self):
+        self.assertEqual(self.p1 * self.p2, Polynomial([1, 0, 2, 10, 29, 30]))
+
+    def test_mul_error(self):
+        self.assertRaises(ValueError, self.p1.__mul__, "x")
+
+    def test_rmul_number(self):
+        self.assertEqual(100 * self.p1, Polynomial([100, 200, 300]))
+
+    def test_rmul_error(self):
+        self.assertRaises(ValueError, self.p1.__rmul__, "x")
+
+    def test_eq(self):
+        self.assertEqual(self.p1==Polynomial([1, 2, 3]), True)
+
+    def test_ne(self):
+        self.assertEqual(self.p1!=Polynomial([1, 2, 0]), True)
+
+    def test_lt(self):
+        self.assertEqual(self.p1<self.p2, True)
+
+    def test_le(self):
+        self.assertEqual(self.p1<=self.p2 and self.p1<=self.p1, True)
+
+    def test_gt(self):
+        self.assertEqual(self.p2>self.p1, True)
+
+    def test_ge(self):
+        self.assertEqual(self.p2>=self.p1 and self.p2>=self.p2, True)
+
+    def test_calc_zero(self):
+        self.assertEqual(self.p0.calc(10), 0)
+
+    def test_calc(self):
+        self.assertEqual(self.p1.calc(10), 123)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ polynomial.py
@@ -0,0 +1,151 @@
+import re
+import itertools
+
+
+class Polynomial:
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+
+    @property
+    def coeffs(self):
+        return self._coeffs
+
+    @coeffs.setter
+    def coeffs(self, cff):
+        copy = list(cff)
+        while copy and copy[0] == 0:
+            del copy[0]
+        for c in copy:
+            if not isinstance(c, int):
+                raise ValueError("Incorrect format of values for polynomial coefficients")
+        if not copy:
+            self._coeffs = (0,)
+        else:
+            self._coeffs = list(copy)
+
+    def __repr__(self):
+        return 'Polynomial({})'.format(self.coeffs)
+
+    def __str__(self):
+        res = ''
+        if self.coeffs == (0,) or self.coeffs == [0, ]:
+            return '0'
+        for i in range(len(self.coeffs) - 1):
+            if (self.coeffs[i] > 0):
+                res += '+ ' + str(self.coeffs[i]) + 'x^' + str(len(self.coeffs) - i - 1) + ' '
+            if (self.coeffs[i] < 0):
+                res += '- ' + str(abs(self.coeffs[i])) + 'x^' + str(len(self.coeffs) - i - 1) + ' '
+        if (self.coeffs[-1] != 0):
+            if (self.coeffs[-1] > 0):
+                res += '+ '
+            else:
+                res += '- '
+            res += str(abs(self.coeffs[-1]))
+        if res == '':
+            return '0'
+        if res[0] == '+':
+            res = res[1:]
+        res = re.sub('x\\^1\\b', 'x', res)
+        return res.replace(' 1x', ' x')
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            c = list(self.coeffs)
+            c[-1] += other
+            return Polynomial(c)
+
+        if isinstance(other, Polynomial):
+            return Polynomial(
+                [x + y for x, y in itertools.zip_longest(reversed(self.coeffs), reversed(other.coeffs), fillvalue=0)][
+                ::-1])
+
+        raise ValueError("Incorrect argument")
+
+    def __radd__(self, other):
+        if isinstance(other, int):
+            c = list(self.coeffs)
+            c[-1] += other
+            return Polynomial(c)
+
+        raise ValueError("Incorrect argument")
+
+    def __sub__(self, other):
+        if isinstance(other, int):
+            c = list(self.coeffs)
+            c[-1] -= other
+            return Polynomial(c)
+
+        if isinstance(other, Polynomial):
+            return Polynomial(
+                [x - y for x, y in itertools.zip_longest(reversed(self.coeffs), reversed(other.coeffs), fillvalue=0)][
+                ::-1])
+
+        raise ValueError("Incorrect argument")
+
+    def __rsub__(self, other):
+        if isinstance(other, int):
+            c = [(-1) * c for c in self.coeffs]
+            c[-1] += other
+            return Polynomial(c)
+
+        raise ValueError("Incorrect argument")
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Polynomial([c * other for c in self.coeffs])
+
+        if isinstance(other, Polynomial):
+            c = [0] * (len(self.coeffs) + len(other.coeffs) - 1)
+            for i1, c1 in enumerate(self.coeffs):
+                for i2, c2 in enumerate(other.coeffs):
+                    c[i1 + i2] += c1 * c2
+            return Polynomial(c)
+
+        raise ValueError("Incorrect argument")
+
+    def __rmul__(self, other):
+        if isinstance(other, int):
+            return Polynomial([c * other for c in self.coeffs])
+
+        raise ValueError("Incorrect argument")
+
+    def __eq__(self, other):
+        return self.coeffs == other.coeffs
+
+    def __lt__(self, other):
+        if len(self.coeffs) == len(other.coeffs):
+            return self.coeffs < other.coeffs
+        else:
+            return len(self.coeffs) < len(other.coeffs)
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.coeffs == other.coeffs
+
+    def __ne__(self, other):
+        return self.coeffs != other.coeffs
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def calc(self, value):
+        result = 0
+        for i in range(len(self.coeffs) - 1):
+            result += self.coeffs[i] * pow(value, len(self.coeffs) - i - 1)
+        return result + self.coeffs[-1]
+
+
+if __name__ == "__main__":
+    p1 = Polynomial([-10, 2, 3, -1, -5, 1, 0, 0, 0, 0, 0, 0, 0, 1])
+    p2 = Polynomial([1, 2])
+    p4 = Polynomial([1, 2])
+
+    print(p1)
+    print(p2)
+    print(p4)
+    print(p2.__repr__())
+    print(-1 * p1)
+    print(p2 <= p4)
+    print(p2.calc(2))
